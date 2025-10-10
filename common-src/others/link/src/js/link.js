83300462e020c319e35542597725df227f4142ae
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-10 20:44:09 +0900

    edit some files

---
+++ common-src/others/link/src/js/link.js
@@ -0,0 +1,40 @@
+window.fetch("-:-JSON-URL-:-").then(res => res.json()).then(dt => {
+	const ol_title = document.getElementById("ol-title");
+	const base_url = "https://link.tshuto.com";
+	const type_arr = [
+		["number", _dt => {
+			const _tar_obj = _dt["info"]["range"];
+			const _arr_len = Number(_tar_obj[1]) - Number(_tar_obj[0]) + 1;
+			let _url_arr = [];
+			for (let i = 0; i < _arr_len; i++)
+				_url_arr.push(new URL(new URL(`${dt["basename"]}${i + Number(_tar_obj[0])}${dt["ext"]}`, dt["path"]), base_url).href);
+			return _url_arr;
+		}],
+		["string", _dt => {
+			const _tar_obj = _dt["info"]["range"];
+			let _url_arr = [];
+			for (let i = 0; i < _tar_obj.length; i++)
+				_url_arr.push(new URL(new URL(`${dt["basename"]}${_tar_obj[i]}${dt["ext"]}`, dt["path"]), base_url).href);
+			return _url_arr;
+		}],
+		["free", _dt => {
+			const _tar_obj = _dt["info"]["files"];
+			let _url_arr = [];
+			for (let i = 0; i < _tar_obj.length; i++)
+				_url_arr.push(_tar_obj[i]);
+			return _url_arr;
+		}]];
+	if (!dt.hasOwnProperty("info") || !dt["info"].hasOwnProperty("type") || (dt.hasOwnProperty("info") && dt["info"].hasOwnProperty("type") && !type_arr.some(c => c[0] == dt["info"]["type"]))) {
+		document.body.appendChild(createL("Access denied", false));
+	} else {
+		for (let v of type_arr)
+			if (v[0] == dt["info"]["type"])
+				[...v[1](dt)].forEach(c => document.body.appendChild(createL(c)));
+	}
+});
+
+function createL(inner_text = "", with_a_element = true) {
+	const el = document.createElement("l");
+	el.innerHTML = with_a_element ? `<a href="${inner_text}" download>${inner_text}</a>` : inner_text;
+	return el;
+}
