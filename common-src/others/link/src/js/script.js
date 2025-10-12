ce9e6acf72c22027cea5db8d780594d23bd9cdb2
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-12 21:22:49 +0900

    edit some files

--- common-src/others/link/src/js/script.js
+++ common-src/others/link/src/js/script.js
@@ -1,7 +1,13 @@
 const main_sel = document.getElementById("main-sel");
 
-function createOptionElement() {
+fetch("https://link.tshuto.com/get-json").then(res => res.json()).then(dt => {
+	for (let i = 0; i < dt.length; i++)
+		main_sel.appendChild(dt[i]);
+});
+
+function createOptionElement(val) {
 	const el = document.createElement("option");
-	
+	el.value = val;
+	el.innerHTML = val;
 	return el;
 }
