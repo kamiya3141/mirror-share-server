6a606be6f1907bcdeb17c2de3dd4c34020504d7a
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-12 21:52:17 +0900

    edit some files

--- common-src/others/link/src/js/script.js
+++ common-src/others/link/src/js/script.js
@@ -2,12 +2,5 @@ const main_sel = document.getElementById("main-sel");
 
 fetch("https://link.tshuto.com/get-json").then(res => res.json()).then(dt => {
 	for (let i = 0; i < dt.length; i++)
-		main_sel.appendChild(createOptionElement(dt[i]));
+		main_sel.innerHTML = [...dt].reduce((s, c) => s + `<option value="${c}">${c}</option>`, "");
 });
-
-function createOptionElement(val) {
-	const el = document.createElement("option");
-	el.value = val;
-	el.innerHTML = val;
-	return el;
-}
