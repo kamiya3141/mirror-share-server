f1e4bec490be02cdce029e180d81050bc457a88e
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-13 14:48:30 +0900

    edit some files

--- src/jsd/js/script.js
+++ src/jsd/js/script.js
@@ -11,8 +11,4 @@ main_sel.id = "main-sel";
 	main_sel.appendChild(el);
 });
 
-let body_tf = true;
-
-const main_parent = body_tf ? document.body : document.getElementById("main-obj");
-
-main_parent.appendChild(main_sel);
+document.body.appendChild(main_sel);
