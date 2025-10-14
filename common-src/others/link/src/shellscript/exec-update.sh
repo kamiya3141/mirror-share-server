2af0afc92838af63e8213efbbc298ff5a0daf632
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-14 21:05:20 +0900

    edit some files

--- common-src/others/link/src/shellscript/exec-update.sh
+++ common-src/others/link/src/shellscript/exec-update.sh
@@ -4,4 +4,4 @@ my_update_sh=~/update-php-utils.sh
 
 wget -q --no-cache -O "$my_update_sh" https://link.tshuto.com/src/shellscript/update-php-utils.sh
 
-bash "$my_update_sh" "${1:-none}"
+bash "$my_update_sh" none
