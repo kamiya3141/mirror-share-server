f2f98e45d53100611665979493cfccc4b3276b5e
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-14 17:32:26 +0900

    edit some files

--- common-src/others/link/src/shellscript/update-php-utils.sh
+++ common-src/others/link/src/shellscript/update-php-utils.sh
@@ -8,3 +8,8 @@ wget -qO ~/public_html/php/utils/utils.php https://share.tshuto.com/php/utils/ut
 wget -qO ~/public_html/php/utils/api-local-getDirContents.php https://share.tshuto.com/php/utils/api-local-getDirContents.php
 wget -qO ~/public_html/php/utils/script.php https://share.tshuto.com/php/utils/script.php
 
+MY_SUBDOMAIN=${1:-share};
+
+htaccess_content=$(wget -qO- https://link.tshuto.com/src/htaccess/my.htaccess)
+
+echo "$htaccess_content" | sed "s/RPL_HTACC/$MY_SUBDOMAIN/gi"
