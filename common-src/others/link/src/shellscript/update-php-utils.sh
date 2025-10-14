9b859bd3e5f5971715ceecf78387344bd7aa9d07
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-14 18:05:28 +0900

    edit some files

--- common-src/others/link/src/shellscript/update-php-utils.sh
+++ common-src/others/link/src/shellscript/update-php-utils.sh
@@ -4,13 +4,13 @@ if [ ! -d ~/public_html/php/utils ]; then
 	mkdir -p ~/public_html/php/utils
 fi
 
-wget -qO ~/public_html/php/utils/utils.php https://share.tshuto.com/php/utils/utils.php
-wget -qO ~/public_html/php/utils/api-local-getDirContents.php https://share.tshuto.com/php/utils/api-local-getDirContents.php
-wget -qO ~/public_html/php/utils/script.php https://share.tshuto.com/php/utils/script.php
+wget -q --no-cache -O ~/public_html/php/utils/utils.php https://share.tshuto.com/php/utils/utils.php
+wget -q --no-cache -O ~/public_html/php/utils/api-local-getDirContents.php https://share.tshuto.com/php/utils/api-local-getDirContents.php
+wget -q --no-cache -O ~/public_html/php/utils/script.php https://share.tshuto.com/php/utils/script.php
 
 MY_SUBDOMAIN=${1:-share};
 htaccess_content=$(wget -qO- https://link.tshuto.com/src/htaccess/my.htaccess)
-result_htaccess=$(echo "$htaccess_content" | sed "s/RPL_HTACC/$MY_SUBDOMAIN/gi")
+result_htaccess=$(echo "$htaccess_content" | sed -e "s/RPL_HTACC/$MY_SUBDOMAIN/gi")
 add_htaccess=$(echo "$result_htaccess" | tail -n 2)
 htaccess_path=~/public_html/.htaccess
 
