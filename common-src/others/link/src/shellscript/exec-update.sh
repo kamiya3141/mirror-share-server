b5a09cf6149ad75dff4b03b39423164c57f9e543
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-14 18:10:41 +0900

    edit some files

---
+++ common-src/others/link/src/shellscript/exec-update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+my_update_sh=~/update-php-utils.sh
+
+wget -q --no-cache -O "$my_update_sh" https://link.tshuto.com/src/shellscript/update-php-utils.sh
+
+bash "$my_update_sh" "${1:-share}"
