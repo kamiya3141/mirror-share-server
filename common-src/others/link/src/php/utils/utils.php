68ed3502dfc1e16446b7b8aa98d1c874581a85bb
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-15 21:14:21 +0900

    edit some files

--- common-src/others/link/src/php/utils/utils.php
+++ common-src/others/link/src/php/utils/utils.php
@@ -21,7 +21,7 @@ define('API_URL', [
 	'getdir' => url_join(UTILS_DIR, 'api-local-getDirContents.php')
 ]);
 
-$mySubDomain = 'share';
+$mySubDomain = 'MYSUBDOMAIN';
 
 $mimeMap = [
 	'js' => 'application/javascript',
