5fa557ab26c27b512d8a367e1eb0cc22f41cb7f2
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-15 23:04:37 +0900

    edit some files

--- common-src/others/link/src/js/script.js
+++ common-src/others/link/src/js/script.js
@@ -4,3 +4,7 @@ fetch("https://link.tshuto.com/get-json").then(res => res.json()).then(dt => {
 	for (let i = 0; i < dt.length; i++)
 		main_sel.innerHTML = [...dt].reduce((s, c) => s + `<option value="${c}">${c}</option>`, "");
 });
+
+const btn = document.getElementById("go-button");
+
+btn.mouseClickHandler = (me, e) => window.location.href = `https://link.tshuto.com/${main_sel.value}`;
