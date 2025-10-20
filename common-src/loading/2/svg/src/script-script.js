bacd0d595d3988b3b2ac317ac366da5455c49adc
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-20 13:34:05 +0900

    edit some files

--- common-src/loading/2/svg/src/script-script.js
+++ common-src/loading/2/svg/src/script-script.js
@@ -3,16 +3,6 @@ parentElement.setAttribute("my-attr-jump-href", "false");
 
 parentElement.setAttribute("viewBox", `0 0 ${width} ${height}`);
 
-const animateInfoArray = [
-	["x1", `0;${width};0;${width};0`],
-	["x2", `${width};0;${width};0;${width}`],
-	["y1", `0;0;${height};${height};0`],
-	["y2", `0;${height};${height};0;0`]
-];
-animateInfoArray.forEach(c =>
-	document.querySelector(`animate[attributeName=${c[0]}]`).setAttribute("values", c[1])
-);
-
 let frame_count = 0;
 let loading_text_id;
 const loading_text = ["Loading...", "Completed!"];
@@ -20,7 +10,7 @@ let SWITCH_FLAG = false;
 
 function setup() {
 	frame_count = 0;
-	parentElement.innerHTML += `<text id="loading-text-id" class="loading-text-class" text-anchor="middle" x="${width /
+	parentElement.innerHTML += `<image href="https://share.tshuto.com/favicon.ico" x="${width / 2 - common_small_r * 3}" y="${height / 4 - common_small_r * 3}" width="${common_small_r * 6}" height="${common_small_r * 6}"></image><text id="loading-text-id" class="loading-text-class" text-anchor="middle" x="${width /
 		2}" y="${height / 2}" font-size="${common_small_r * 3}" fill="blue"></text>`;
 
 	loading_text_id = document.getElementById("loading-text-id");
@@ -43,56 +33,14 @@ window.addEventListener("load", () => {
 		if (frame_count == 360) frame_count = 0;
 
 		if (SWITCH_FLAG == true && loading_text_id.innerHTML == loading_text[0]) {
-			loading_text_id.classList.remove("before");
-			loading_text_id.classList.add("after");
-			loading_text_id.innerHTML = loading_text[1];
+			//loading_text_id.classList.remove("before");
+			//loading_text_id.classList.add("after");
+			//loading_text_id.innerHTML = loading_text[1];
 			setTimeout(function () {
 				//setup();
 				parentElement.setAttribute("class", "loading animation-end");
 				window.parent.postMessage("end", "*");
-			}, 3000);
+			}, 1500);
 		}
 	}, 1000 / frame_rate);
 });
-function hsbToRgb(h, s, b, alpha = "1") {
-	s /= 100;
-	b /= 100;
-	let c = b * s;
-	let x = c * (1 - Math.abs(h / 60 % 2 - 1));
-	let m = b - c;
-	let r = 0,
-		g = 0,
-		bl = 0;
-
-	if (0 <= h && h < 60) {
-		r = c;
-		g = x;
-		bl = 0;
-	} else if (60 <= h && h < 120) {
-		r = x;
-		g = c;
-		bl = 0;
-	} else if (120 <= h && h < 180) {
-		r = 0;
-		g = c;
-		bl = x;
-	} else if (180 <= h && h < 240) {
-		r = 0;
-		g = x;
-		bl = c;
-	} else if (240 <= h && h < 300) {
-		r = x;
-		g = 0;
-		bl = c;
-	} else if (300 <= h && h < 360) {
-		r = c;
-		g = 0;
-		bl = x;
-	}
-
-	r = Math.round((r + m) * 255);
-	g = Math.round((g + m) * 255);
-	bl = Math.round((bl + m) * 255);
-
-	return `rgba(${r},${g},${bl},${alpha})`;
-}
