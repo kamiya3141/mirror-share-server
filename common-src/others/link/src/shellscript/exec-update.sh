d2221845ec68b88b9a4e9de2b3b2ed1948f995a7
kamiya3141 <kamiyatuwu@gmail.com>
2025-10-23 10:44:53 +0900

    edit some files

--- common-src/others/link/src/shellscript/exec-update.sh
+++ common-src/others/link/src/shellscript/exec-update.sh
@@ -3,5 +3,9 @@
 my_update_sh=~/update-php-utils.sh
 my_update_remote_url="https://raw.githubusercontent.com/kamiya3141/mirror-share-server/refs/heads/main/common-src/others/link/src"
 
-wget -q --no-cache -O "$my_update_sh" https://raw.githubusercontent.com/kamiya3141/mirror-share-server/refs/heads/main/common-src/others/link/src/shellscript/update-php-utils.sh
+if [ "$MY_UPDATE_REMOTE_URL" ]; then
+	my_update_remote_url="$MY_UPDATE_REMOTE_URL"
+fi
+
+wget -q --no-cache -O "$my_update_sh" "$my_update_remote_url"/shellscript/update-php-utils.sh
 bash "$my_update_sh" none "$my_update_remote_url"
