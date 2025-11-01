9bed81234a1cd67d5ea335f9c9abcebd8e5f9909
kamiya3141 <kamiyatuwu@gmail.com>
2025-11-01 13:00:14 +0900

    update link.tshuto.com/src/js/script.js

--- common-src/others/link/src/js/script.js
+++ common-src/others/link/src/js/script.js
@@ -1,7 +1,7 @@
 const main_sel = document.getElementById("main-sel");
 
 fetch("https://link.tshuto.com/get-dirs/src/").then(res => res.json()).then(dt => {
-	main_sel.innerHTML = [...dt].reduce((s, c) => s + `<option value="${c}">${c}</option>`, "");
+	main_sel.innerHTML = [...dt].map(c => c.replace("/", "")).reduce((s, c) => s + `<option value="${c}">${c}</option>`, "");
 });
 
 const btn = document.getElementById("go-button");
