b45129158ca101c5ae921245a09946ceba9df3bc
kamiya3141 <kamiyatuwu@gmail.com>
2025-11-01 17:52:58 +0900

    edit some files

--- common-src/html/link/src/js/script.js
+++ common-src/html/link/src/js/script.js
@@ -1,4 +1,5 @@
-const NO_YAJU_QUERY_NAME = "yj";
+const NO_YAJU_QUERY_NAME_ARRAY = ["yj", "yaju", "yajuu"];
+const NO_ARTICLE_QUERY_NAME = "atc";
 const YAJU_VALUE = "niKAylKNIEI";
 
 const MAIN_URL = "-:-JSON-URL-:-";
@@ -9,14 +10,19 @@ const DIRS_URL = MAIN_URL.replace("get-files/", "get-dirs/");
 
 const FREE_LINK_JSON_FILENAME = "link";
 
-const yajuExist = (new URLSearchParams((new URL(window.location.toString())).search)).has(NO_YAJU_QUERY_NAME);
+const current_query = new URLSearchParams((new URL(window.location.toString())).search);
+const yajuExist = NO_YAJU_QUERY_NAME_ARRAY.some(c => current_query.has(c));
+const notArticleFlagExist = current_query.has(NO_ARTICLE_QUERY_NAME);
 const main_ol = document.getElementById("main-ol");
 const sub_ol = document.getElementById("sub-ol");
 
 // 広告用のYoutubeリンクのクエリパラメータのキー "v" の値を格納した配列
 const youtube_v_array = [YAJU_VALUE, "39sjhHJrPLA", "6uv0rhZgDy0", "xi1Wk4kt1mA"].reverse();
-// 広告の差込み処理
-youtube_v_array.forEach(v => sub_ol.appendChild(createElement_li_articleYoutube(v)));
+// 広告拒否ではないのなら広告を貼る
+if (notArticleFlagExist)
+	sub_ol.appendChild(createElement_li_articleYoutube(youtube_v_array.at(-1)));
+else
+	youtube_v_array.forEach(v => sub_ol.appendChild(createElement_li_articleYoutube(v)));
 // YAJU & U の消滅処理
 if (yajuExist)
 	document.getElementById(YAJU_VALUE).parentElement.style.setProperty("display", "none", "important");
@@ -25,7 +31,6 @@ if (yajuExist)
 window.fetch(MAIN_URL).then(res => res.json()).then(dt0 => {
 	if (checkHasLength(dt0)) {
 		let dt = deleteSlashInArray(dt0);
-		console.log(dt);
 		if (dt.includes(FREE_LINK_JSON_FILENAME)) {
 			dt.splice(dt.indexOf(FREE_LINK_JSON_FILENAME), 1);
 			window.fetch(`${MAIN_BASE_URL}/${FREE_LINK_JSON_FILENAME}.json`).then(res => res.json()).then(dt2 => {
@@ -42,26 +47,44 @@ window.fetch(DIRS_URL).then(res => res.json()).then(dt => {
 	if (checkHasLength(dt)) {
 		deleteSlashInArray(dt).forEach(c => {
 			const _url = `${MAIN_URL + c}/`;
-			console.log(_url);
 			window.fetch(_url).then(res => res.json()).then(dt2 => main_ol.appendChild(createElement_ol_block(c, deleteSlashInArray(dt2).map(c2 => `${_url.replace("get-files/", "")}${c2}`))));
 		});
 	}
 });
 
+if (notArticleFlagExist) {
+	const tsm = document.querySelector(`ts-message[color-type="normal"]`);
+	tsm.remove();
+}
+if (yajuExist) {
+	console.log(yajuExist);
+	const tsm = document.querySelector(`ts-message[color-type="alert"]`);
+	tsm.remove();
+}
+
+function moveNonArticlePage() {
+	const new_url = new URL(window.location.toString());
+	new_url.searchParams.set(NO_ARTICLE_QUERY_NAME, "");
+	if (yajuExist)
+		new_url.searchParams.set(NO_YAJU_QUERY_NAME_ARRAY[0], "");
+	window.location.href = new_url;
+}
+
+function moveNonYajuPage() {
+	const new_url = new URL(window.location.toString());
+	new_url.searchParams.set(NO_YAJU_QUERY_NAME_ARRAY[0], "");
+	if (notArticleFlagExist)
+		new_url.searchParams.set(NO_ARTICLE_QUERY_NAME, "");
+	window.location.href = new_url;
+}
+
 
 function checkHasLength(obj = []) {
 	return (Object.hasOwn(obj, "length") && [...obj].length > 0);
 }
-
-/**
- * 
- * @param {Array.<String>} arr
- * @returns {Array.<String>} 文字列 格納 配列
- */
 function deleteSlashInArray(arr = []) {
 	return arr.map(c => c.replace("/", ""));
 }
-
 function addChildLiElement(prt, _dt = []) {
 	if (checkHasLength(_dt))
 		[..._dt].forEach(c => prt.appendChild(createElement_li(c)));
