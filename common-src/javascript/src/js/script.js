ce614b10905567cc73bf2d012cad8cd15fdbe520
kamiya3141 <kamiyatuwu@gmail.com>
2025-11-07 23:30:29 +0900

    edit some files

--- common-src/javascript/src/js/script.js
+++ common-src/javascript/src/js/script.js
@@ -6,10 +6,18 @@ const sandboxIframeWindow = sandboxIframe.contentWindow;
 
 const cacheThemeJsonData = {
 	"default": {
-		"vs": "",
-		"hc-light": "",
-		"vs-dark": "",
-		"hc-black": ""
+		"vs": {
+			"name": "vs"
+		},
+		"hc-light": {
+			"name": "hc-light"
+		},
+		"vs-dark": {
+			"name": "vs-dark"
+		},
+		"hc-black": {
+			"name": "hc-black"
+		}
 	},
 	"light": {
 		"Amy": "",
@@ -71,6 +79,80 @@ const cacheThemeJsonData = {
 		"Pastels on Dark": ""
 	}
 };
+const themeNameDataObject = {
+	"active4d": "Active4D",
+	"all-hallows-eve": "All Hallows Eve",
+	"amy": "Amy",
+	"birds-of-paradise": "Birds of Paradise",
+	"blackboard": "Blackboard",
+	"brilliance-black": "Brilliance Black",
+	"brilliance-dull": "Brilliance Dull",
+	"chrome-devtools": "Chrome DevTools",
+	"clouds-midnight": "Clouds Midnight",
+	"clouds": "Clouds",
+	"cobalt": "Cobalt",
+	"cobalt2": "Cobalt2",
+	"dawn": "Dawn",
+	"dracula": "Dracula",
+	"dreamweaver": "Dreamweaver",
+	"eiffel": "Eiffel",
+	"espresso-libre": "Espresso Libre",
+	"github-dark": "GitHub Dark",
+	"github-light": "GitHub Light",
+	"github": "GitHub",
+	"idle": "IDLE",
+	"katzenmilch": "Katzenmilch",
+	"kuroir-theme": "Kuroir Theme",
+	"lazy": "LAZY",
+	"magicwb--amiga-": "MagicWB (Amiga)",
+	"merbivore-soft": "Merbivore Soft",
+	"merbivore": "Merbivore",
+	"monokai-bright": "Monokai Bright",
+	"monokai": "Monokai",
+	"night-owl": "Night Owl",
+	"nord": "Nord",
+	"oceanic-next": "Oceanic Next",
+	"pastels-on-dark": "Pastels on Dark",
+	"slush-and-poppies": "Slush and Poppies",
+	"solarized-dark": "Solarized-dark",
+	"solarized-light": "Solarized-light",
+	"spacecadet": "SpaceCadet",
+	"sunburst": "Sunburst",
+	"textmate--mac-classic-": "Textmate (Mac Classic)",
+	"tomorrow-night-blue": "Tomorrow-Night-Blue",
+	"tomorrow-night-bright": "Tomorrow-Night-Bright",
+	"tomorrow-night-eighties": "Tomorrow-Night-Eighties",
+	"tomorrow-night": "Tomorrow-Night",
+	"tomorrow": "Tomorrow",
+	"twilight": "Twilight",
+	"upstream-sunburst": "Upstream Sunburst",
+	"vibrant-ink": "Vibrant Ink",
+	"xcode-default": "Xcode_default",
+	"zenburnesque": "Zenburnesque",
+	"iplastic": "iPlastic",
+	"idlefingers": "idleFingers",
+	"krtheme": "krTheme",
+	"monoindustrial": "monoindustrial"
+};
+
+Object.keys(themeNameDataObject).forEach(monacoOnlyThemeName => {
+	const themeName = themeNameDataObject[monacoOnlyThemeName];
+	Object.keys(cacheThemeJsonData).forEach(labelName => {
+		if (cacheThemeJsonData[labelName].hasOwnProperty(themeName))
+			cacheThemeJsonData[labelName][themeName] = {
+				"name": monacoOnlyThemeName,
+				"data": ""
+			};
+	});
+});
+
+const PRIMARY_THEME_KIND_NAME = "default";
+
+let __debug__theme_check_arr = [];
+
+/* --- 全部先にロードする --- */
+const ALLOW_ALL_THEME_LOAD = true;
+
 
 const themeSetSelectElement = document.getElementById("theme-set-sel");
 
@@ -79,17 +161,24 @@ const themeSetSelectElement = document.getElementById("theme-set-sel");
 	Object.keys(cacheThemeJsonData).forEach(labelName => {
 		const optgroupElement = document.createElement("optgroup");
 		optgroupElement.label = __FORCE_UPPER_CASE ? labelName.toUpperCase() : labelName;
-		cacheThemeJsonData[labelName].keys().forEach(optionValue => {
+		Object.keys(cacheThemeJsonData[labelName]).forEach(optionValue => {
 			const optionElement = document.createElement("option");
 			optionElement.value = optionValue;
 			optionElement.textContent = optionValue;
-			if (labelName == "default" && optionValue == `vs${checkCurrentSystemThemeLight() ? "" : "-dark"}`)
+			if (labelName == PRIMARY_THEME_KIND_NAME && optionValue == `vs${checkCurrentSystemThemeLight() ? "" : "-dark"}`)
 				optionElement.selected = true;
 			optgroupElement.appendChild(optionElement);
 		});
 		themeSetSelectElement.appendChild(optgroupElement);
 	});
 })();
+
+
+window.console.log = (...input) => {
+	input = input.map(s => String(s).replaceAll("\n", "<br>"));
+	consoleResult.innerHTML = `${input.join("<br>")}`;
+};
+sandboxIframe.contentWindow.console.log = (...input) => console.log(...input);
 
 require.config({
 	paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs" }
@@ -99,7 +188,7 @@ require(["vs/editor/editor.main"], () => {
 	const editor = monaco.editor.create(codeBody, {
 		value: ``,
 		language: "javascript",
-		theme: "vs-dark",
+		theme: themeSetSelectElement.options[themeSetSelectElement.selectedIndex].value,
 		fontSize: "18",
 		// fontFamily: "'Explex-Regular', sans-serif",
 		fontLigatures: true,
@@ -114,18 +203,36 @@ require(["vs/editor/editor.main"], () => {
 	});
 
 	async function loadTheme(themeName) {
-		const themeAttribute = Object.keys(cacheThemeJsonData).find(c => cacheThemeJsonData[c][themeName]);
-
-		const themeData = cacheThemeJsonData[themeAttribute][themeName].length > 0 ?
-			cacheThemeJsonData[themeAttribute][themeName] :
-			(await (await fetch(`https://cdn.jsdelivr.net/npm/monaco-themes/themes/${themeName}.json`))().json());
-
-		if (themeAttribute != "default") monaco.editor.defineTheme(themeName, themeData);
-
-		monaco.editor.setTheme(themeName);
+		const themeAttribute = Object.keys(cacheThemeJsonData).find(c => cacheThemeJsonData[c].hasOwnProperty(themeName)) || "null";
+		const resultThemeName = cacheThemeJsonData[themeAttribute][themeName]["name"];
+		if (themeAttribute != PRIMARY_THEME_KIND_NAME) {
+			const themeData = cacheThemeJsonData[themeAttribute][themeName]["data"] ? cacheThemeJsonData[themeAttribute][themeName]["data"] : (await (async function () {
+				const res = await fetch(`https://cdn.jsdelivr.net/npm/monaco-themes/themes/${themeName}.json`);
+				const jsonData = await res.json();
+				cacheThemeJsonData[themeAttribute][themeName]["data"] = jsonData;
+				return jsonData;
+			})());
+			monaco.editor.defineTheme(resultThemeName, themeData);
+		}
+		monaco.editor.setTheme(resultThemeName);
 	}
+	themeSetSelectElement.addEventListener("change", e => {
+		loadTheme(themeSetSelectElement.options[themeSetSelectElement.selectedIndex].value);
+	});
+
+	if (ALLOW_ALL_THEME_LOAD)
+		[...themeSetSelectElement.options].forEach(opt => {
+			__debug__theme_check_arr.push(opt.value);
+			loadTheme(opt.value);
+		});
+	console.log(__debug__theme_check_arr.join("\n"));
+
+	loadTheme(themeSetSelectElement.options[themeSetSelectElement.selectedIndex].value);
+
 	editor.onDidChangeModelContent(e => {
-		const code = String(editor.getValue()).replace(new RegExp("document", "g"), `document.getElementById("sandbox-iframe").contentWindow.document`);
+		let code = String(editor.getValue());
+		code = code.replace(new RegExp("document|window.document", "g"), `document.getElementById("sandbox-iframe").contentWindow.document`);
+		code = code.replace(new RegExp("window", "g"), `document.getElementById("sandbox-iframe").contentWindow`);
 		try {
 			document.getElementById("sandbox-iframe").contentWindow.document.body.innerHTML = "";
 			eval(code);
@@ -133,7 +240,4 @@ require(["vs/editor/editor.main"], () => {
 			console.log(error);
 		}
 	});
-
-	window.console.log = (...input) => consoleResult.innerHTML = `${input.join("<br>")}`;
-	sandboxIframe.contentWindow.console.log = (...input) => console.log(...input);
 });
