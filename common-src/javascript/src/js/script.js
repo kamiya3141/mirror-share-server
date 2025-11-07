b27ac8b7f93d94066d72807e3f9b448a00f6a96c
kamiya3141 <kamiyatuwu@gmail.com>
2025-11-07 18:44:45 +0900

    edit some files

--- common-src/javascript/src/js/script.js
+++ common-src/javascript/src/js/script.js
@@ -4,16 +4,96 @@ const consoleResult = document.getElementById("console-result");
 
 const sandboxIframeWindow = sandboxIframe.contentWindow;
 
-const cacheThemeJsonData = new Object();
+const cacheThemeJsonData = {
+	"default": {
+		"vs": "",
+		"hc-light": "",
+		"vs-dark": "",
+		"hc-black": ""
+	},
+	"light": {
+		"Amy": "",
+		"Chrome DevTools": "",
+		"Clouds": "",
+		"Dawn": "",
+		"Dreamweaver": "",
+		"Eiffel": "",
+		"GitHub Light": "",
+		"GitHub": "",
+		"IDLE": "",
+		"iPlastic": "",
+		"Katzenmilch": "",
+		"Kuroir Theme": "",
+		"LAZY": "",
+		"MagicWB (Amiga)": "",
+		"Solarized-light": "",
+		"Textmate (Mac Classic)": "",
+		"Tomorrow": "",
+		"Xcode_default": ""
+	},
+	"dark": {
+		"Active4D": "",
+		"All Hallows Eve": "",
+		"Birds of Paradise": "",
+		"Blackboard": "",
+		"Brilliance Black": "",
+		"Brilliance Dull": "",
+		"Clouds Midnight": "",
+		"Cobalt": "",
+		"Cobalt2": "",
+		"Dominion Day": "",
+		"Dracula": "",
+		"Espresso Libre": "",
+		"GitHub Dark": "",
+		"idleFingers": "",
+		"Merbivore Soft": "",
+		"Merbivore": "",
+		"monoindustrial": "",
+		"Monokai Bright": "",
+		"Monokai": "",
+		"Night Owl": "",
+		"Nord": "",
+		"Oceanic Next": "",
+		"Slush and Poppies": "",
+		"Solarized-dark": "",
+		"SpaceCadet": "",
+		"Sunburst": "",
+		"Tomorrow-Night": "",
+		"Tomorrow-Night-Blue": "",
+		"Tomorrow-Night-Bright": "",
+		"Tomorrow-Night-Eighties": "",
+		"Twilight": "",
+		"Upstream Sunburst": "",
+		"Vibrant Ink": "",
+		"Zenburnesque": ""
+	},
+	"other": {
+		"Pastels on Dark": ""
+	}
+};
+
+const themeSetSelectElement = document.getElementById("theme-set-sel");
+
+(function () {
+	const __FORCE_UPPER_CASE = true;
+	Object.keys(cacheThemeJsonData).forEach(labelName => {
+		const optgroupElement = document.createElement("optgroup");
+		optgroupElement.label = __FORCE_UPPER_CASE ? labelName.toUpperCase() : labelName;
+		cacheThemeJsonData[labelName].keys().forEach(optionValue => {
+			const optionElement = document.createElement("option");
+			optionElement.value = optionValue;
+			optionElement.textContent = optionValue;
+			if (labelName == "default" && optionValue == `vs${checkCurrentSystemThemeLight() ? "" : "-dark"}`)
+				optionElement.selected = true;
+			optgroupElement.appendChild(optionElement);
+		});
+		themeSetSelectElement.appendChild(optgroupElement);
+	});
+})();
 
 require.config({
 	paths: { vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs" }
 });
-
-console.log(getComputedStyle(document.documentElement).fontSize);
-
-window.console.log = (...input) => consoleResult.innerHTML = `${input.join("<br>")}`;
-sandboxIframe.contentWindow.console.log = (...input) => console.log(...input);
 
 require(["vs/editor/editor.main"], () => {
 	const editor = monaco.editor.create(codeBody, {
@@ -34,13 +114,16 @@ require(["vs/editor/editor.main"], () => {
 	});
 
 	async function loadTheme(themeName) {
-		const themeData = cacheThemeJsonData.hasOwnProperty(themeName) ?
-			cacheThemeJsonData[themeName] :
+		const themeAttribute = Object.keys(cacheThemeJsonData).find(c => cacheThemeJsonData[c][themeName]);
+
+		const themeData = cacheThemeJsonData[themeAttribute][themeName].length > 0 ?
+			cacheThemeJsonData[themeAttribute][themeName] :
 			(await (await fetch(`https://cdn.jsdelivr.net/npm/monaco-themes/themes/${themeName}.json`))().json());
-		monaco.editor.defineTheme(themeName, themeData);
+
+		if (themeAttribute != "default") monaco.editor.defineTheme(themeName, themeData);
+
 		monaco.editor.setTheme(themeName);
 	}
-
 	editor.onDidChangeModelContent(e => {
 		const code = String(editor.getValue()).replace(new RegExp("document", "g"), `document.getElementById("sandbox-iframe").contentWindow.document`);
 		try {
@@ -50,4 +133,7 @@ require(["vs/editor/editor.main"], () => {
 			console.log(error);
 		}
 	});
+
+	window.console.log = (...input) => consoleResult.innerHTML = `${input.join("<br>")}`;
+	sandboxIframe.contentWindow.console.log = (...input) => console.log(...input);
 });
