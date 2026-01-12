cfa2df545534fb8fa39a61b038e9e4aea48bda7c
kamiya3141 <kamiyatuwu@gmail.com>
2026-01-12 14:30:47 +0900

    edit some files

--- common-src/blog/src/js/script.js
+++ common-src/blog/src/js/script.js
@@ -1,7 +1,7 @@
 async function mainFunc() {
 	const CORRECT_URL = ".";// await fetch(`${winMyHrefPTCHostname}?convert=true`).then(res => res.text());
 	const PMD = await import(`${CORRECT_URL}/index-module.js`);
-	await PMD.parseMDv1();
+	await PMD.parseMD();
 }
 
 window.addEventListener("load", mainFunc);
