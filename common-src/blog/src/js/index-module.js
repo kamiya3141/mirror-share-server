1c8735baafa470be876e6913c918a61a6081ef3b
kamiya3141 <kamiyatuwu@gmail.com>
2026-02-19 16:45:00 +0900

    edit some files

--- common-src/blog/src/js/index-module.js
+++ common-src/blog/src/js/index-module.js
@@ -141,6 +141,69 @@ const before_replace_str_define_array = [
 		cts => `<${cts}>`,
 		null,
 		false
+	],
+	[
+		/:::\snote(\n[\s\S]*?):::/g,
+		cts => createNoteInnerHTMLString("info", cts),
+		null,
+		null,
+		null
+	],
+	[
+		/:::\snote\sinfo(\n[\s\S]*?\n):::/g,
+		cts => createNoteInnerHTMLString("info", cts),
+		null,
+		null,
+		null
+	],
+	[
+		/:::\snote\swarn(\n[\s\S]*?\n):::/g,
+		cts => createNoteInnerHTMLString("warn", cts),
+		null,
+		null,
+		null
+	],
+	[
+		/:::\snote\salert(\n[\s\S]*?\n):::/g,
+		cts => createNoteInnerHTMLString("alert", cts),
+		null,
+		null,
+		null
+	]
+];
+
+const before_replace_str_define_array_test = [
+	[
+		new RegExp(`(${SAND_SPL_STR("MYHOSTNAME")})`, "g"),
+		hostname => winMyHrefHostname,
+		null,
+		null,
+		null
+	],
+	[
+		/```([^\n]*?):([^\n]*?)\n([\s\S]*?)```/g,
+		(nm, cts) => createCodeInnerHTMLString("line-multi code-frame-normal code-iframe-common-styles", nm, cts),
+		[2, 3],
+		null,
+		null
+	],
+	[
+		/```([^\n]*?)\n([\s\S]*?)```/g,
+		(nm, cts) => createCodeInnerHTMLString("line-multi code-frame-normal code-iframe-common-styles", nm, cts),
+		[1, 2],
+		null,
+		null
+	],
+	[
+		/```([^\r\n]*?)```/g,
+		cts => createCodeInnerHTMLString("line-solo code-frame-mini", "none", cts, true),
+		null,
+		null,
+		null
+	],
+	[
+		/`([^\r\n]*?)`/g,
+		cts => createCodeInnerHTMLString("line-solo code-frame-mini", "none", cts, true)
 	]
 ];
 
@@ -175,7 +238,9 @@ async function parseMarkDown2HTMLContextVersion1(mdurl = "") {
 	const mdtxt = await fetch(mdurl).then(res => res.text());
 	let result_str = mdtxt;
 
-	for (let v of before_replace_str_define_array) {
+	const BEFORE_REPLACE_STR_DEFINE_ARRAY = before_replace_str_define_array_test;
+
+	for (let v of BEFORE_REPLACE_STR_DEFINE_ARRAY) {
 		const result_array = [...result_str.matchAll(v[0])];
 		if (result_array.length) {
 			for (let chv of result_array) {
