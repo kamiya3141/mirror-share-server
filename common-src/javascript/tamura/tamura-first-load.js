5110c4bcdf74d161416a870e9e890c28052f7e92
kamiya3141 <kamiyatuwu@gmail.com>
2026-02-19 16:23:59 +0900

    edit some files

--- common-src/javascript/tamura/tamura-first-load.js
+++ common-src/javascript/tamura/tamura-first-load.js
@@ -79,7 +79,7 @@ var winMySrcFileBasePath = WINV["mySourceFileBasePath"];
 
 var checkCurrentSystemThemeLight = () => Boolean(!window.matchMedia("(prefers-color-scheme: dark)").matches);
 var checkCurrentSystemThemeDark = () => !checkCurrentSystemThemeLight();
-var useOldUserAgentDataValue = true;
+var useOldUserAgentDataValue = false;
 var checkCurrentDeviceMobile = () => Boolean((new RegExp("Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini", "i")).test(useOldUserAgentDataValue ? navigator.userAgent : (navigator.userAgentData ? (navigator.userAgentData.mobile ? "Android" : "PC") : navigator.userAgent)));
 var checkCurrentDevicePC = () => !checkCurrentDeviceMobile();
 
