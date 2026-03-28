b62883f702c0d8ff4ed38f7755a9441eb7b983fb
kamiya3141 <kamiyatuwu@gmail.com>
2026-03-28 15:53:11 +0900

    edit some files

--- common-src/blog/src/js/script.js
+++ common-src/blog/src/js/script.js
@@ -10,3 +10,10 @@ async function mainFunc() {
 }
 
 window.addEventListener("load", mainFunc);
+
+document.getElementById("open-setting-display-button-element").addEventListener("click", switchingOpenSettingDisplay, true);
+
+function switchingOpenSettingDisplay(e) {
+	const data = document.getElementById("setting-display-section").getAttribute("data-setting-menu-open");
+	document.getElementById("setting-display-section").setAttribute("data-setting-menu-open", data == "false" ? "true" : "false");
+}
