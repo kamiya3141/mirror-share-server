6d96939f854ae930ed7fc7f62d4c0a89187f032e
kamiya3141 <kamiyatuwu@gmail.com>
2026-04-02 16:18:34 +0900

    edit some files

--- common-src/blog/src/js/script.js
+++ common-src/blog/src/js/script.js
@@ -12,23 +12,36 @@ async function mainFunc() {
 	const displayElementQueryArray = [
 		{
 			"trigger-element": ["#open-setting-display-button-element", "#setting-display-div-main #control-box"],
-			"focus-out-element": "#setting-display-div-main",
+			"focus-out-element": "#setting-display-div-main .display-item-box",
 			"switched-element": "#setting-display-section"
 		}
 	];
 
 	displayElementQueryArray.forEach(obj => {
-		obj["trigger-element"].forEach(el => document.querySelector(el).addEventListener("click", e => switchingOpenDisplay(obj["switched-element"])));
-		document.querySelector(obj["focus-out-element"]).addEventListener("blur", e => switchingOpenDisplay(obj["switched-element"]));
+		let switched_elem = document.querySelector(obj["switched-element"]);
+		switched_elem.tabIndex = 0;
+		obj["trigger-element"].forEach(el => {
+			document.querySelector(el).addEventListener("click", e => {
+				switchingOpenDisplay(switched_elem);
+			}, true);
+		});
+		document.querySelector(obj["focus-out-element"]).addEventListener("focusout", e => {
+			switchingOpenDisplay(switched_elem);
+			console.log("ok!!");
+		}, true);
 	});
 }
 
 window.addEventListener("load", mainFunc);
 
 
-function switchingOpenDisplay(query = "") {
+function switchingOpenDisplay(elem = "") {
 	const attr_name = "data-display-open";
-	const data = document.querySelector(query).getAttribute(attr_name);
-	console.log(data);
-	document.querySelector(query).setAttribute(attr_name, (data == "false" ? "true" : "false"));
+	const data = elem.getAttribute(attr_name);
+	let data_is_true = Boolean(data == "true");
+
+	if (!data_is_true)
+		elem.focus();
+
+	elem.setAttribute(attr_name, (data_is_true ? "false" : "true"));
 }
