d2532ba48268f07b15c1f1723895ab0cf0b2fb35
kamiya3141 <kamiyatuwu@gmail.com>
2026-04-05 09:33:51 +0900

    edit some files

--- common-src/tso/src/js/script.js
+++
@@ -1,9 +0,0 @@
-async function mainFunc() {
-	const CORRECT_URL = ".";// await fetch(`${winMyHrefPTCHostname}?convert=true`).then(res => res.text());
-	const PMD = await import(`${CORRECT_URL}/index-module.js`);
-	if (winMyHrefPathname == "/")
-		window.location.href = `${winMyHrefPTCHostname}/home`;
-	await PMD.parseMD();
-}
-
-window.addEventListener("load", mainFunc);
