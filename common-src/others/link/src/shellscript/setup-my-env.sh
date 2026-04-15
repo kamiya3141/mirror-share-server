0e92d85d323f9b758626fc6d0226b0a527bdc867
kamiya3141 <kamiyatuwu@gmail.com>
2026-04-15 09:59:58 +0900

    edit some files

--- common-src/others/link/src/shellscript/setup-my-env.sh
+++ common-src/others/link/src/shellscript/setup-my-env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+profile_path="/home/${USER}/.profile"
 bashrc_path="/home/${USER}/.bashrc"
 bash_aliases_path="/home/${USER}/.bash_aliases"
+my_bashrc_path="/home/${USER}/my.bashrc"
+my_0_bash_aliases_path="/home/${USER}/my-0.bash_aliases"
 
 my_update_cache="/home/${USER}/.update-utils"
 
@@ -14,23 +17,65 @@ wget --no-cache -qO - https://raw.githubusercontent.com/kamiya3141/mirror-share-
 if [ -f "${bashrc_path}" ]; then
 	bashrc_contents=$(cat "${bashrc_path}")
 	if [[ "${bashrc_contents}" != *"my.bashrc"* ]]; then
-		echo -e "\nif [ -f ~/my.bashrc ]; then\n\tsource ~/my.bashrc\nfi\n\nexport TERM=xterm-256color" >> "${bashrc_path}"
+		echo -e "\nif [ -f ~/my.bashrc ]; then\n\t. ~/my.bashrc\nfi\nif [ -f ~/my-*.bash_aliases ]; then\n\t. ~/my-*.bash_aliases\nfi\n\nexport TERM=xterm-256color" >> "${bashrc_path}"
 	fi
 fi
 
-# . "${bashrc_path}"
-# . "${bash_aliases_path}"
 
-shopt -s expand_aliases
-source ~/.bashrc
 
-wgmbr 9
-
-apis locales
+sudo apt install -y locales
 sudo dpkg-reconfigure locales
+
 
 echo -e "\nexport LANG=ja_JP.UTF-8\nexport LC_ALL=ja_JP.UTF-8\n\nclear\n" >> "${bashrc_path}"
 
-up-apt
+
+sudo apt install -y parted rsync curl gpg mc screen
+
+
+sudo mc
+mc
+
+
+wget https://kamiya3141.github.io/mirror-share-server/.hide/.debian/.screenrc -O ~/.screenrc
+
+echo -e '\nif [ -z "$STY" ] && [ "$TERM" != "dumb" ]; then\n\tscreen -xRR\nfi' | sudo tee -a "${profile_path}"
+
+
+sudo touch "${my_0_bash_aliases_path}"
+sudo chmod 755 "${my_0_bash_aliases_path}"
+echo -e 'alias rmba=". ~/my-*.bash_aliases && clear"\nalias emba="sudo nano ~/my-0.bash_aliases && rmba"' | sudo tee -a "${my_0_bash_aliases_path}"
+
+wget --no-cache -qO - https://raw.githubusercontent.com/kamiya3141/mirror-share-server/refs/heads/main/common-src/others/link/src/bashrc/my-7.bashrc >> "${my_bashrc_path}"
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y autoremove
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+
+
+read -p "cloudflaredをインストールしますか？	(Y) / N: " -n 1 _val
+_val=${_val:-Y}
+case "$_val" in
+	Y|y)
+		_val="Y"
+	;;
+	*)
+		_val="B"
+	;;
+esac
+
+if [[ $_var == Y ]]; then
+	echo -e "\nYesが選択されました"
+	sudo mkdir -p --mode=0755 /usr/share/keyrings
+	curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+	echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+	sudo apt update
+	sudo apt -y install cloudflared
+	echo -e "\nCloudflaredのインストールが終了しました\n"
+fi
+
 
 echo -e "\a\nfinished !!\n"
