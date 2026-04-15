90e0ad0c2181801f496a6df0c41d7a919883fbe1
kamiya3141 <kamiyatuwu@gmail.com>
2026-04-15 11:31:19 +0900

    edit some files

--- common-src/others/link/src/js/blog/loader.js
+++ common-src/others/link/src/js/blog/loader.js
@@ -17,7 +17,7 @@ const scriptSettingObjectArray = [
 		"type": "text/javascript"
 	}
 	// ↓ カンマ演算子使ってるよ、読みづらいね(笑)
-].map(scriptSettingObject => (scriptSettingObject["src"] = joinBaseHostname(scriptSettingObject["src"]), scriptSettingObject["defer"] = "", scriptSettingObject)).forEach(scriptSettingObject => {
+].map(scriptSettingObject => (scriptSettingObject["defer"] = "", scriptSettingObject["src"] = joinBaseHostname(scriptSettingObject["src"]), scriptSettingObject)).forEach(scriptSettingObject => {
 	const scriptElement = document.createElement("script");
 	Object.entries(scriptSettingObject).forEach(([attributeName, attributeValue]) => scriptElement.setAttribute(attributeName, attributeValue));
 	document.body.appendChild(scriptElement);
