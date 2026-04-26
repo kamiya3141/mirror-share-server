c1e533df909c7c43d7ca341afbb637015a2c3a9f
kamiya3141 <kamiyatuwu@gmail.com>
2026-04-26 11:48:59 +0900

    edit some files

--- common-src/others/link/src/js/blog/loader.js
+++ common-src/others/link/src/js/blog/loader.js
@@ -1,23 +1,37 @@
 const joinBaseHostname = pathname => (pathname = String(pathname), `${winMyHrefPTCHostname}${(pathname[0] != "/" ? "/" : "")}${pathname}`);
-const scriptSettingObjectArray = [
-	{
-		"src": "src/js/device-info.js",
-		"type": "text/javascript"
-	},
-	{
-		"src": "src/js/setup.js",
-		"type": "text/javascript"
-	},
-	{
-		"src": "src/js/script.js",
-		"type": "module"
-	},
-	{
-		"src": "src/js/after.js",
-		"type": "text/javascript"
-	}
-	// ↓ カンマ演算子使ってるよ、読みづらいね(笑)
-].map(scriptSettingObject => (scriptSettingObject["defer"] = "", scriptSettingObject["src"] = joinBaseHostname(scriptSettingObject["src"]), scriptSettingObject)).forEach(scriptSettingObject => {
+const allScriptSettingObjectArrayObject = {
+	"main": [
+		{
+			"src": "src/js/device-info.js",
+			"type": "text/javascript"
+		},
+		{
+			"src": "src/js/setup.js",
+			"type": "text/javascript"
+		},
+		{
+			"src": "src/js/script.js",
+			"type": "module"
+		},
+		{
+			"src": "src/js/after.js",
+			"type": "text/javascript"
+		}
+	],
+	"embed": [
+		{
+			"src": "src/js/embed.js",
+			"type": "text/javascript"
+		}
+	]
+};
+
+
+
+let resultScriptSettingObjectArray = allScriptSettingObjectArrayObject["main"];
+
+// ↓ カンマ演算子使ってるよ、読みづらいね(笑)
+resultScriptSettingObjectArray.map(scriptSettingObject => (scriptSettingObject["defer"] = "", scriptSettingObject["src"] = joinBaseHostname(scriptSettingObject["src"]), scriptSettingObject)).forEach(scriptSettingObject => {
 	const scriptElement = document.createElement("script");
 	Object.entries(scriptSettingObject).forEach(([attributeName, attributeValue]) => scriptElement.setAttribute(attributeName, attributeValue));
 	document.body.appendChild(scriptElement);
