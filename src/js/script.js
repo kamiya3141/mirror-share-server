95103d251b32be19904e4b4e9601620c5cd75599
kamiya3141 <kamiyatuwu@gmail.com>
2026-05-16 09:36:49 +0900

    edit some files

--- src/js/script.js
+++ src/js/script.js
@@ -39,7 +39,7 @@ function reloadWindowInfo() {
 	console.log(typeof _arr[0][1], _arr[0][1], typeof _arr[2][1], _arr[2][1], typeof _arr[1][1], _arr[1][1]);
 
 	const _pushed_idx = Number(Boolean(_arr[1][1] == _arr[2][1]));
-	_arr.push([_br + _br + `${_arr[2][0]} = ${_arr[_pushed_idx][0]}`.replace(_br, "")]);
+	_arr.push([_br + _br + `${_arr[3][0]} = ${_arr[_pushed_idx][0]}`.replace(_br, "")]);
 	// この繋げ方は思いついたとき、最高な気分になった
 	document.querySelector("div#div-box").innerHTML = `<div>${_arr.map((c, i, a) => c.join(__joinning_str) + `${(i + 1) < a.length ? __joinning_str : ""}${_br}${_br}`).join("</div><div>")}</div>`;
 }
