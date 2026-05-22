609057887c293d2446057ea32e58dbb86ec422ba
kamiya3141 <kamiyatuwu@gmail.com>
2026-05-22 09:51:05 +0900

    edit some files

--- common-src/sites/blog/src/js/create-new-md-page.js
+++ common-src/sites/blog/src/js/create-new-md-page.js
@@ -7,10 +7,17 @@ var appear_createNewArticleSettingDisplay = async (_tf, _pmd) => {
 
 function settingNewArticleSettingDisplay(p_e, _pmd) {
 	const fm_el = p_e.querySelector("#create-new-article--form");
-	fm_el.addEventListener("submit", e => {
+	fm_el.addEventListener("submit", async e => {
 		e.preventDefault();
-		const data = Object.fromEntries(new FormData(e.target));
-		console.log(data);
-
+		const form_data = new FormData(e.target);
+		console.log(form_data);
+		const _res = await fetch(_pmd.createAPIURL("article-new-api-local.php"),
+			{
+				"method": "POST",
+				"body": form_data
+			}
+		);
+		const _dt = await _res.json();
+		alert(_dt);
 	});
 }
