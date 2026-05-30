69a9d6c5f408781910530f1972bd74aecca5c1cf
kamiya3141 <kamiyatuwu@gmail.com>
2026-05-30 10:36:33 +0900

    edit some files

--- .hide/streamdeck/src/js/script.js
+++ .hide/streamdeck/src/js/script.js
@@ -7,6 +7,12 @@ function createButton(input_src = "") {
 	btn_el.type = "button";
 	const img_el = document.createElement("img");
 	img_el.src = convertEnvVars(input_src);
+	const div_el = document.createElement("div");
+	div_el.classList.add("expanded-clear-element");
+	div_el.addEventListener("click", e => {
+		
+	});
 	btn_el.appendChild(img_el);
+	btn_el.appendChild(div_el);
 	return btn_el;
 }
