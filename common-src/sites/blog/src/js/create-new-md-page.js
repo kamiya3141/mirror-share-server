e9dc70540ae7f5cad9a9a4782b125f63bc930d1b
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-05 11:15:22 +0900

    edit some files

--- common-src/sites/blog/src/js/create-new-md-page.js
+++ common-src/sites/blog/src/js/create-new-md-page.js
@@ -67,11 +67,10 @@ function settingNewArticleSettingDisplay(p_e, _pmd, decoded_json_data) {
 		});
 		const _dt = await _res.json();
 		if (_dt["success"] && (button_type == "created" || button_type == "updated")) {
-			console.log(_dt);
 			const next_url = new URL(`${winMyHrefPTCHostname}`);
 			next_url.searchParams.set(page_flag[0], "");
-			next_url.searchParams.set(id_flag, _dt["slug"]);
-			window.location.href = next_url;
+			next_url.searchParams.set(id_flag, (has_id_flag ? data["slug"] : _dt["slug"]));
+			// window.location.href = next_url;
 		} else
 			console.log(_dt);
 	});
