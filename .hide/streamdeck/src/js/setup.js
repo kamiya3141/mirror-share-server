52a513cc838765d93c44f7cc05c79a399bbbcb43
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-06 12:39:24 +0900

    edit some files

--- .hide/streamdeck/src/js/setup.js
+++ .hide/streamdeck/src/js/setup.js
@@ -23,8 +23,12 @@ const MY_FUNCTIONS = {
 	"set-cols-7": () => {
 		[["GridCols", 7], ["IconSize", 12]].forEach(c => document.documentElement.style.setProperty(`--myStylingLocal${c[0]}`, String(c[1])));
 	},
-	"open-ws": () => socketActivate(),
-	"close-ws": () => socketClose()
+	"open-ws": () => {
+		socketActivate();
+	},
+	"close-ws": () => {
+		socketClose();
+	}
 };
 let CURRENT_STACKED_DIR_DEPTH = [];
 let DEFINE_JSON_DIR = {
