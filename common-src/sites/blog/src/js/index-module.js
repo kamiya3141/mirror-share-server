f4e5f663d6f6541741de705233352acbbec7e195
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-14 10:04:23 +0900

    edit some files

--- common-src/sites/blog/src/js/index-module.js
+++ common-src/sites/blog/src/js/index-module.js
@@ -165,12 +165,13 @@ const before_replace_str_define_array = [
 		(alt, url, ttl, rdm = Math.floor(Math.random() * (10 ** 12))) => `
 		<button class="img-box" commandfor="md-dialog-${rdm}" command="show-modal">
 			<img src="${url}" title="${ttl ? ttl : url}" alt="${alt ? alt : url}">
-		</button><dialog id="md-dialog-${rdm}">
+		</button>
+		<dialog id="md-dialog-${rdm}">
 			<div class="md-dialog-close-button-box">
 				<button commandfor="md-dialog-${rdm}" command="close">CLOSE</button>
 			</div>
 			<img src="${url}" title="${ttl ? ttl : url}" alt="${alt ? alt : url}">
-		</dialog>`,
+		</dialog>`.replaceAll("\n", "").replaceAll("\t", ""),
 		[1, 2, 3]
 	],
 	[
