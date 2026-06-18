b21fcf1b700025cf9ef860d139f73c0803ae6eb6
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-19 08:38:24 +0900

    edit some files

--- common-src/sites/beta-blog/src/js/display-all-articles.js
+++ common-src/sites/beta-blog/src/js/display-all-articles.js
@@ -14,22 +14,31 @@ var appear_allArticlesDisplay = async (_tf, all_article_data_object = []) => {
 function createArticleCard(article_data_object) {
 	const all_articles_contents_box = my_root_section.querySelector(".article-contents--box");
 
-
 	const card_div_element = document.createElement("div");
 	card_div_element.classList.add("article-card");
 	card_div_element.classList.add("cursor-pointer");
 	Object.entries(article_data_object).forEach(([k, v]) => card_div_element.setAttribute(`data-mydef--article-card--${k}`, v));
 
+	// info-box
+	const card_info_button_box_div_element = document.createElement("div");
+	card_info_button_box_div_element.classList.add("article-card--info-box");
+	// info
+	const card_info_button_div_element = document.createElement("div");
+	card_info_button_div_element.classList.add("article-card--info");
+	card_info_button_div_element.innerHTML = "&#133";
+	// embed
 	const card_embed_div_element = document.createElement("div");
 	card_embed_div_element.classList.add("artcile-card--embed");
 	card_embed_div_element.innerHTML = article_data_object["excerpt"];
-
+	// title
 	const card_title_div_element = document.createElement("div");
 	card_title_div_element.classList.add("artcile-card--title");
 	card_title_div_element.innerHTML = article_data_object["title"];
 
 	card_div_element.appendChild(card_embed_div_element);
 	card_div_element.appendChild(card_title_div_element);
+	card_info_button_box_div_element.appendChild(card_info_button_div_element);
+	card_div_element.appendChild(card_info_button_box_div_element);
 
 	card_div_element.addEventListener("click", e => {
 		const next_url = new URL(winMyHref);
