51455bb89b6f48d6bfc735139e372dd1e84ba7f8
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-21 11:36:30 +0900

    edit some files

--- common-src/sites/blog/src/js/display-all-articles.js
+++ common-src/sites/blog/src/js/display-all-articles.js
@@ -14,26 +14,67 @@ var appear_allArticlesDisplay = async (_tf, all_article_data_object = []) => {
 function createArticleCard(article_data_object) {
 	const all_articles_contents_box = my_root_section.querySelector(".article-contents--box");
 
-
 	const card_div_element = document.createElement("div");
 	card_div_element.classList.add("article-card");
 	card_div_element.classList.add("cursor-pointer");
 	Object.entries(article_data_object).forEach(([k, v]) => card_div_element.setAttribute(`data-mydef--article-card--${k}`, v));
 
+
+	// info-item-box
+	const card_info_item_box_div_rdm = createRDM();
+	const card_info_item_box_div_element = document.createElement("div");
+	card_info_item_box_div_element.classList.add("article-card--info-item-box");
+	card_info_item_box_div_element.id = `article-card--info-item-box-${card_info_item_box_div_rdm}`;
+	card_info_item_box_div_element.setAttribute("popover", "auto");
+	// info-item
+	[["id", "インデックス"], ["title", "タイトル"], ["slug", "ID"], ["file_name", "ファイル名"], ["category", "カテゴリ"], ["tags", "タグ"], ["excerpt", "見出し"], ["created_at", "作成日"], ["updated_at", "最終更新日"]].forEach(c => {
+		const card_info_item_div_element = document.createElement("button");
+		card_info_item_div_element.classList.add("article-card--info-item");
+		card_info_item_div_element.setAttribute("popovertarget", card_info_item_box_div_element.id);
+		card_info_item_div_element.innerHTML = `記事の${c[1]}をコピー`;
+		card_info_item_div_element.addEventListener("click", async e => {
+			const tf = Object.hasOwn(article_data_object, c[0]);
+			if (tf)
+				await navigator.clipboard.writeText(article_data_object[c[0]]);
+			myAlertMessage(c[1] + tf ? "をコピーしました。" : "が存在しません。");
+		});
+		card_info_item_box_div_element.appendChild(card_info_item_div_element);
+	});
+
+	// info-button-box
+	const card_info_button_box_div_element = document.createElement("div");
+	card_info_button_box_div_element.classList.add("article-card--info-button-box");
+	// info-button
+	const card_info_button_div_element = document.createElement("button");
+	card_info_button_div_element.classList.add("article-card--info--button");
+	addEventPopoverElementsMini(card_info_button_div_element, card_info_item_box_div_element, true);
+	const _dev_tp = document.documentElement.getAttribute("data-my-device-type");
+	// デスクトップなら ⋯ を、モバイルなら長押し or 右クリック
+	if (_dev_tp == "desktop")
+		card_info_button_div_element.innerHTML += "&ctdot;";
+
+	// embed
 	const card_embed_div_element = document.createElement("div");
 	card_embed_div_element.classList.add("artcile-card--embed");
 	card_embed_div_element.innerHTML = article_data_object["excerpt"];
-
+	// title
 	const card_title_div_element = document.createElement("div");
 	card_title_div_element.classList.add("artcile-card--title");
 	card_title_div_element.innerHTML = article_data_object["title"];
 
 	card_div_element.appendChild(card_embed_div_element);
 	card_div_element.appendChild(card_title_div_element);
+	card_info_button_div_element.appendChild(card_info_item_box_div_element);
+	card_info_button_box_div_element.appendChild(card_info_button_div_element);
+	card_div_element.appendChild(card_info_button_box_div_element);
 
+	card_div_element.addEventListener("contextmenu", e => {
+		e.preventDefault();
+		card_info_button_div_element.click();
+	});
 	card_div_element.addEventListener("click", e => {
 		const next_url = new URL(winMyHref);
-		next_url.searchParams.set(id_flag, e.target.getAttribute("data-mydef--article-card--slug"));
+		next_url.searchParams.set(id_flag, card_div_element.getAttribute("data-mydef--article-card--slug"));
 		window.location.href = next_url;
 	});
 
