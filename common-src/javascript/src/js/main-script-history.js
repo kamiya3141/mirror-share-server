e8fdde852f7b0f1e02a1920f7e3182eb074b6851
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-21 12:51:38 +0900

    edit some files

--- common-src/javascript/src/js/main-script-history.js
+++
@@ -1,3 +0,0 @@
-const a = new URL("https://abc.ex.com/abc/def");
-
-console.log(a.hostname, a.pathname);
--- common-src/javascript/src/js/script.js
+++ common-src/javascript/src/js/script.js
@@ -11,6 +11,9 @@ for (let ifr_id of templateElementArray) {
 	document.body.prepend(clone);
 }
 */
+
+WINV["mySourceFileBasePathArrayIndex"] = 2;
+
 const codeBody = document.getElementById("code-body");
 const sandboxIframe = document.getElementById("sandbox-iframe");
 const consoleResult = document.getElementById("console-result");
