cbccd3bf11fd19cd20185e6e13b270cff389a745
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-24 08:02:42 +0900

    edit some files

--- common-src/javascript/src/js/script.js
+++ common-src/javascript/src/js/script.js
@@ -145,27 +145,25 @@ require(["vs/editor/editor.main"], () => {
 		}
 	});
 	function saveMainScriptHitoryForRemoteFile(tf = false) {
+		/*
 		const url = new URL("https://share.tshuto.com/common-src/javascript/src/php/server.php");
 		url.searchParams.set("rewrite-script-file-path", "js/main-script-history.js");
 		url.searchParams.set("rewrite-script-file-open-mode", "w");
 		url.searchParams.set("data", cacheMainScriptHistoryData);
-		/*
-		"https://share.tshuto.com/common-src/javascript/src/php/server.php?rewrite-script-file-path=js/main-script-history.js&rewrite-script-file-open-mode=w", {
+		fetch(url)
+		*/
+		fetch("https://share.tshuto.com/common-src/javascript/src/php/server.php?rewrite-script-file-path=js/main-script-history.js&rewrite-script-file-open-mode=w", {
 			"method": "POST",
-			"headers": {
-				"Content-Type": "application/json"
-			},
 			"body": JSON.stringify({
+				"data-type": "php-input",
 				"data": cacheMainScriptHistoryData
 			})
-		}
-		*/
-		fetch(url).then(res => res.text()).then(dt => {
-			if (dt != "true")
-				console.log(dt);
-			if (tf)
-				editor.setValue(cacheMainScriptHistoryData);
-		});
+		}).then(res => res.text()).then(dt => {
+				if (dt != "true")
+					console.log(dt);
+				if (tf)
+					editor.setValue(cacheMainScriptHistoryData);
+			});
 	}
 	codeBody.addEventListener("keydown", e => {
 		if (e.ctrlKey && String(e.key).toLowerCase() == "s") {
--- common-src/javascript/src/php/server.php
+++ common-src/javascript/src/php/server.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once '/home/tamura/public_html/php/utils/utils.php';
+
 $target_script_path = $_GET["rewrite-script-file-path"];
 $target_script_open_mode = $_GET["rewrite-script-file-open-mode"];
 if ($target_script_path == "js/main-script-history.js" && $target_script_open_mode == "w") {
