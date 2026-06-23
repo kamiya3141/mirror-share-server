ae4c4e6c7982e04ea604ce4a9c1ed639c558e338
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-24 08:13:05 +0900

    edit some files

--- common-src/javascript/src/php/server.php
+++ common-src/javascript/src/php/server.php
@@ -7,7 +7,7 @@ $target_script_open_mode = $_GET["rewrite-script-file-open-mode"];
 if ($target_script_path == "js/main-script-history.js" && $target_script_open_mode == "w") {
 	$scriptData = $php_input ? json_decode($php_input, true) : "";
 
-	$result = file_put_contents(('/home/tamura/common-src/javascript/src/' .  $target_script_path), $scriptData);
+	$result = file_put_contents(('./../' .  $target_script_path), $scriptData);
 	// echo ($result ? "true" : "false");
-	echo json_encode($php_input);
+	echo ($result ? "true" : "false") . " : " . json_encode($php_input);
 }
