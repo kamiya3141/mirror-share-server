275a6db0c4ac9fd4d31229bbe99f7c1a71839c10
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-24 12:00:31 +0900

    edit some files

--- common-src/javascript/src/php/server.php
+++ common-src/javascript/src/php/server.php
@@ -7,7 +7,9 @@ $target_script_open_mode = $_GET["rewrite-script-file-open-mode"];
 $result_script_path = './../../' .  $target_script_path;
 if ($target_script_open_mode == "w") {
 	$scriptData = $php_input ? $php_input : "";
-	$result = file_put_contents($result_script_path, $scriptData);
+	$result = false;
+	if ($php_input)
+		$result = file_put_contents($result_script_path, $scriptData);
 	$last_result = ($result ? "true" : "false");
 	echo $last_result;
 	// echo $last_result . " : " . $scriptData;
