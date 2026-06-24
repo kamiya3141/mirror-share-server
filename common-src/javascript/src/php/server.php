65dff5aebf66849b71f36337b4cda46ed1d60309
kamiya3141 <kamiyatuwu@gmail.com>
2026-06-24 09:28:22 +0900

    edit some files

--- common-src/javascript/src/php/server.php
+++ common-src/javascript/src/php/server.php
@@ -4,12 +4,13 @@ require_once '/home/tamura/public_html/php/utils/utils.php';
 
 $target_script_path = $_GET["rewrite-script-file-path"];
 $target_script_open_mode = $_GET["rewrite-script-file-open-mode"];
+$result_script_path = './../../' .  $target_script_path;
 if ($target_script_open_mode == "w") {
 	$scriptData = $php_input ? json_decode($php_input, true) : "";
 
-	$result = file_put_contents(('./../../' .  $target_script_path), $scriptData);
+	$result = file_put_contents($result_script_path, $scriptData);
 	// echo ($result ? "true" : "false");
 	echo ($result ? "true" : "false") . " : " . $scriptData;
 } else if ($target_script_open_mode == "r")
-	echo file_get_contents(('./../../' .  $target_script_path));
+	echo file_get_contents($result_script_path);
 exit;
