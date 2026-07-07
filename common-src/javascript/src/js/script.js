6df96a03069d0ac506a0c2e0a0bc9b5e3a229ab7
kamiya3141 <kamiyatuwu@gmail.com>
2026-07-07 11:14:53 +0900

    edit some files

--- common-src/javascript/src/js/script.js
+++ common-src/javascript/src/js/script.js
@@ -253,7 +253,7 @@ require(["vs/editor/editor.main"], () => {
 	pageFontSetSelectElement.addEventListener("change", e => {
 		console.log(getSelectedValueInSelectElement(pageFontSetSelectElement));
 		editDeviceInformation("font-family", getSelectedValueInSelectElement(pageFontSetSelectElement));
-		reloadDeviceInformation();
+		reloadDeviceInformation("page-font-set-select-element");
 	});
 
 	useTabEditorSettingSetSelectElement.addEventListener("change", e => {
