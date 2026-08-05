0cc6c1dd846524f1b1fcceca14b550b1e406a9c0
kamiya3141 <kamiyatuwu@gmail.com>
2026-08-05 14:17:26 +0900

    edit some files

--- common-src/javascript/utils/utils.js
+++ common-src/javascript/utils/utils.js
@@ -486,8 +486,7 @@ function createScrollButtonDivElement(dir = "", btn_txt = "", root) {
 		if (!img_box)
 			return console.error("img_boxが存在しません");
 		img_box.scrollBy({
-			"left": img_box.clientWidth * 0.25 * ((dir.includes("right") ? 1 : -1)),
-			"behavior": "smooth"
+			"left": img_box.clientWidth * 0.25 * ((dir.includes("right") ? 1 : -1))
 		});
 	});
 	return div;
