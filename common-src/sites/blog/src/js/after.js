ade307380f997edd33e7f3feb1f21a44e7fcad04
kamiya3141 <kamiyatuwu@gmail.com>
2026-09-03 10:35:52 +0900

    edit some files

--- common-src/sites/blog/src/js/after.js
+++ common-src/sites/blog/src/js/after.js
@@ -1,7 +1,3 @@
-window[winMyHrefHostname] = {};
-window[winMyHrefHostname]["loaded-array"] = [];
-const afterjs_idx = window[winMyHrefHostname]["loaded-array"].push(0);
-
 const id_flag = "id";
 const edit_flag = "edit";
 const new_flag = "new";
@@ -9,12 +5,9 @@ const new_flag = "new";
 window.addEventListener("load", async () => {
 	if (!hasFlag("create-cache") && hasFlag(id_flag) && getFlag(id_flag).split("--").length == 2 && getFlag(id_flag).split("--")[1] == "articles")
 		await loadAllArticles();
-	else
-		window[winMyHrefHostname]["loaded-array"][afterjs_idx - 1] = 1;
 });
 async function loadAllArticles() {
 	const PMD = await import(`./markdown.js`);
 	const all_decoded_json_data = await PMD.getAllArticleData();
 	await appear_allArticlesDisplay(true, all_decoded_json_data);
-	window[winMyHrefHostname]["loaded-array"][afterjs_idx - 1] = 1;
 }
--- common-src/sites/blog/src/js/script.js
+++ common-src/sites/blog/src/js/script.js
@@ -1,5 +1,3 @@
-const scriptjs_idx = window[winMyHrefHostname]["loaded-array"].push(0);
-
 async function mainFunc() {
 	const PMD = await import(`./markdown.js`);
 	const result = await PMD.parseMD();
@@ -8,8 +6,6 @@ async function mainFunc() {
 	mdContentsBoxElement.innerHTML = result;
 
 	PMD.afterFunction();
-
-	window[winMyHrefHostname]["loaded-array"][scriptjs_idx - 1] = 1;
 }
 
 window.addEventListener("load", mainFunc);
