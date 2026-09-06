893f94eae62f8fda04a3b1fbb9ba3120c43f65da
kamiya3141 <kamiyatuwu@gmail.com>
2026-09-06 12:21:59 +0900

    edit some files

--- common-src/javascript/utils/utils-after.js
+++ common-src/javascript/utils/utils-after.js
@@ -377,6 +377,7 @@ function loadedFunc() {
 		c1["select-option-data-array"].forEach(c2 => {
 			let opt = document.createElement("option");
 			Object.assign(opt, c2);
+			document.getElementById(c1["select-id"]).innerHTML = "";
 			document.getElementById(c1["select-id"]).appendChild(opt);
 		});
 		document.getElementById(c1["select-id"]).addEventListener("change", e => {
