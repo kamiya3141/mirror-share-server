c6f870307e54c8ac0e55da727aeeffc0f7008b61
kamiya3141 <kamiyatuwu@gmail.com>
2026-09-20 10:14:31 +0900

    edit some files

--- common-src/javascript/test/src/js/test-1.js
+++ common-src/javascript/test/src/js/test-1.js
@@ -9,3 +9,11 @@ test1();
 console.log("end");
 
 
+class MyElement {
+	constructor (element) {
+		this.root = element;
+		this.root.innerText = "Hello";
+	}
+}
+
+document.querySelectorAll("myelement").forEach(element => new MyElement(element));
