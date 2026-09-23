8520f35acb4a8fecfb39d8e402e1561f92e0b975
kamiya3141 <kamiyatuwu@gmail.com>
2026-09-23 11:35:39 +0900

    edit some files

--- common-src/sites/react-test/src/AppTest.jsx
+++ common-src/sites/react-test/src/AppTest.jsx
@@ -1,7 +1,7 @@
-function App() {
-    return (
-        <h1>Hello React!</h1>
-    );
+function AppTest() {
+	return (
+		<h1>Hello React!</h1>
+	);
 }
 
-export default App;
+export default AppTest;
